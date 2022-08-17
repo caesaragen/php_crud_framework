c7f1a3e66370c36e6af49abd3b96edf7fcb5f4c6
caesaragen <caesaragen@gmail.com>
2022-08-17 22:12:12 +0300

    fix validation before form submit

--- app/views/addProduct.php
+++ app/views/addProduct.php
@@ -1,5 +1,5 @@
 <?php require APPROOT . '/views/includes/header.php'; ?>
-<form action=" <?php echo URLROOT; ?>/addproduct" ; method="post">
+<form id="product_form" action=" <?php echo URLROOT; ?>/addProduct" ; method="post">
 
     <header>
         <div class="jumbotron">
@@ -7,7 +7,7 @@
                 <div class="d-flex justify-content-between align-items-center">
                     <h1 class="display-4">Product Add</h1>
                     <div class="button-group">
-                        <button class="btn btn-success" type="submit">Save</button>
+                        <button class="btn btn-success" onclick="validateForm()">Save</button>
                         <a class="btn btn-danger" href="<?php echo URLROOT ?>/index">Cancel</a>
                     </div>
                 </div>
@@ -16,34 +16,37 @@
     </header>
     <div class=" container">
         <hr class="my-4">
+        <span class="text-danger" id="input_err"></span>
     </div>
     <div class="container">
         <div class="row mb-3">
-            <label for="sku" class="col-sm-2 col-form-label">SKU</label>
+            <label for="sku" class="col-sm-2 col-form-label">SKU *</label>
             <div class="col-sm-10">
                 <input type="text" name="sku" id="sku" class="form-control
                 <?php echo (!empty($data['sku_err'])) ? 'is-invalid' : ''; ?>">
+                <span class="text-danger" id="sku_err"></span>
             </div>
             <?php echo (!empty($data['sku_err'])) ? '<span class="text-danger">' . $data['sku_err'] . '</span>' : ''; ?>
         </div>
         <div class="row mb-3">
-            <label for="name" class="col-sm-2 col-form-label">Name</label>
+            <label for="name" class="col-sm-2 col-form-label">Name *</label>
             <div class="col-sm-10">
                 <input type="text" name="name" id="name" class="form-control <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>">
                 <?php echo (!empty($data['name_err'])) ? '<span class="text-danger">' . $data['name_err'] . '</span>' : ''; ?>
+                <span class="text-danger" id="name_err"></span>
             </div>
         </div>
         <div class="row mb-3">
-            <label for="price" class="col-sm-2 col-form-label">Price ($)</label>
+            <label for="price" class="col-sm-2 col-form-label">Price ($) *</label>
             <div class="col-sm-10">
                 <input type="text" name="price" id="price" class="form-control <?php echo (!empty($data['price_err'])) ? 'is-invalid' : ''; ?>">
                 <?php echo (!empty($data['price_err'])) ? '<span class="text-danger">' . $data['price_err'] . '</span>' : ''; ?>
-                <i class="text-success">Please, provide Price *</i>
+                <!-- <i class="text-success">Please, provide Price *</i> -->
                 <span class="text-danger" id="price_err"></span>
             </div>
         </div>
         <div class="row mb-3">
-            <label for="type" class="col-sm-2 col-form-label">Type</label>
+            <label for="type" class="col-sm-2 col-form-label">Type *</label>
             <div class="col-sm-10 ">
                 <select class="form-select" name="type" id="productType" aria-label="Default select example" class="form-select <?php echo (!empty($data['type_err'])) ? 'is-invalid' : ''; ?>">
                     <option value="" selected>Type Switcher</option>
@@ -60,7 +63,7 @@
                 <div class="col-sm-10">
                     <input type="text" name="size" id="size" class="form-control <?php echo (!empty($data['size_err'])) ? 'is-invalid' : ''; ?>">
                     <?php echo (!empty($data['size_err'])) ? '<span class="text-danger">' . $data['size_err'] . '</span>' : ''; ?>
-                    <i class="text-success">Please, provide Size *</i>
+                    <!-- <i class="text-success">Please, provide Size *</i> -->
                     <span class="text-danger" id="size_err"></span>
                 </div>
             </div>
@@ -71,7 +74,7 @@
                 <div class="col-sm-10">
                     <input type="text" name="height" id="height" class="form-control <?php echo (!empty($data['height_err'])) ? 'is-invalid' : ''; ?>">
                     <?php echo (!empty($data['height_err'])) ? '<span class="text-danger">' . $data['height_err'] . '</span>' : ''; ?>
-                    <i class="text-success">Please, provide Height *</i>
+                    <!-- <i class="text-success">Please, provide Height *</i> -->
                     <span class="text-danger" id="height_err"></span>
                 </div>
             </div>
@@ -80,7 +83,7 @@
                 <div class="col-sm-10">
                     <input type="text" name="width" id="width" class="form-control <?php echo (!empty($data['width_err'])) ? 'is-invalid' : ''; ?>">
                     <?php echo (!empty($data['width_err'])) ? '<span class="text-danger">' . $data['width_err'] . '</span>' : ''; ?>
-                    <i class="text-success">Please, provide Width *</i>
+                    <!-- <i class="text-success">Please, provide Width *</i> -->
                     <span class="text-danger" id="width_err"></span>
                 </div>
             </div>
@@ -89,7 +92,7 @@
                 <div class="col-sm-10">
                     <input type="text" name="length" id="length" class="form-control <?php echo (!empty($data['length_err'])) ? 'is-invalid' : ''; ?>">
                     <?php echo (!empty($data['length_err'])) ? '<span class="text-danger">' . $data['length_err'] . '</span>' : ''; ?>
-                    <i class="text-success">Please, provide Length *</i>
+                    <!-- <i class="text-success">Please, provide Length *</i> -->
                     <span class="text-danger" id="length_err"></span>
                 </div>
             </div>
@@ -100,7 +103,7 @@
                 <div class="col-sm-10">
                     <input type="text" name="weight" id="weight" class="form-control <?php echo (!empty($data['weight_err'])) ? 'is-invalid' : ''; ?>">
                     <?php echo (!empty($data['weight_err'])) ? '<span class="text-danger">' . $data['weight_err'] . '</span>' : ''; ?>
-                    <i class="text-success">Please, provide weight *</i>
+                    <!-- <i class="text-success">Please, provide weight *</i> -->
                     <span class="text-danger" id="weight_err"></span>
                 </div>
             </div>
@@ -109,38 +112,105 @@
 
 </div>
 <script>
-    // $(document).ready(function() {
-
-    //     $("#product_form").submit(function(e) {
-
-    //         var url = "<?php echo URLROOT; ?>/products/addproduct";
-    //         console.log(url);
-    //         data = {
-    //             sku: $("#sku").val(),
-    //             name: $("#name").val(),
-    //             price: $("#price").val(),
-    //             size: $("#size").val(),
-    //             type: $("#type").val(),
-    //             dvd: $("#dvd").val(),
-    //             height: $("#height").val(),
-    //             width: $("#width").val(),
-    //             length: $("#length").val(),
-    //             weight: $("#weight").val()
-    //         };
-
-    //         $.ajax({
-    //             type: "POST",
-    //             url: url,
-    //             data: data,
-    //             success: function(data) {
-    //                 alert(data);
-    //             }
-    //         });
-    //         e.preventDefault();
-
-    //     });
-    // });
     // validate input fields
+    const validateForm = () => {
+        event.preventDefault();
+
+        var Height = $("#height").val();
+        var Width = $("#width").val();
+        var Length = $("#length").val();
+        var Weight = $("#weight").val();
+        // var Size = $("#size").val();
+        let size = document.getElementById("size").value;
+        let weight = document.getElementById("weight").value;
+        let height = document.getElementById("height").value;
+        let length = document.getElementById("length").value;
+        let width = document.getElementById("width").value;
+        // get selected value from productType select
+        let type = document.getElementById("productType").value;
+        let form = document.getElementById("product_form");
+        let sku = document.getElementById("sku").value;
+        let name = document.getElementById("name").value;
+        let price = document.getElementById("price").value;
+        // console.log(type);
+
+        var isInvalid = $(".is-invalid").length;
+        console.log(isInvalid + " isInvalid");
+        var inputFields = $(".form-control").length;
+        // console.log(inputFields + " inputFields");
+        // console.log(isNaN(height) + " " + width + " " + length + " " + weight + " " + size + "attr");
+
+        // if any  input has is-invalid class, return false
+        if (price == '' || sku == '' || name == '') {
+            $("#input_err").html("Please, provide required inputs *");
+            return false;
+        } else if ($('.is-invalid').length > 0) {
+            return false;
+        } else if (type == 1) {
+            $("#weight").removeClass("is-invalid");
+            $("#height").removeClass("is-invalid");
+            $("#length").removeClass("is-invalid");
+            $("#width").removeClass("is-invalid");
+            $("#weight").val('');
+            $("#height").val('');
+            $("#length").val('');
+            $("#width").val('');
+
+            if (size == '' || isNaN(size)) {
+                $("#size").addClass("is-invalid");
+                $("#size_err").html("Please, provide valid Size *");
+            }
+            // return false
+        } else if (type == 2) {
+            $("#size").removeClass("is-invalid");
+            $("#height").removeClass("is-invalid");
+            $("#length").removeClass("is-invalid");
+            $("#width").removeClass("is-invalid");
+            $("#size").val('');
+            $("height").val('');
+            $("length").val('');
+            $("width").val('');
+            if (weight == '' || isNaN(weight)) {
+                $("#weight").addClass("is-invalid");
+                $("#weight_err").html("Please, provide valid Weight *");
+            }
+            // return false
+        } else if (type == 3) {
+            $("#size").removeClass("is-invalid");
+            $("#weight").removeClass("is-invalid");
+            $("#size").val('');
+            $("weight").val('');
+            if (height == '' || isNaN(height)) {
+                $("#height").addClass("is-invalid");
+                $("#height_err").html("Please, provide valid Height *");
+            }
+            if (width == '' || isNaN(width)) {
+                $("#width").addClass("is-invalid");
+                $("#width_err").html("Please, provide valid Width *");
+            }
+            if (length == '' || isNaN(length)) {
+                $("#length").addClass("is-invalid");
+                $("#length_err").html("Please, provide valid Length *");
+            }
+            // return false
+        } else if (isNaN(height) || isNaN(width) || isNaN(length) || isNaN(weight) || isNaN(size)) {
+            $("#input_err").html("Please, provide valid inputs *");
+            $("#weight").addClass("is-invalid");
+            $("#height").addClass("is-invalid");
+            $("#length").addClass("is-invalid");
+            $("#width").addClass("is-invalid");
+            $("#size").addClass("is-invalid");
+            // return false;
+        } else if (type == "") {
+            $("#input_err").html("Please, select Product Type *");
+            return false;
+        } else {
+            $("#input_err").html("");
+        }
+        $("#input_err").html("");
+        form.submit();
+        return true;
+    }
     $(document).ready(function() {
         var Height = $("#height").val();
         var Width = $("#width").val();
@@ -153,11 +223,13 @@
         var type = $("#type").val();
         var dvd = $("#dvd").val();
 
-
         $("#height").blur(function() {
             if ($("#height").val() == 0) {
                 $("#height").addClass("is-invalid");
                 $("#height_err").html("Height cannot be 0 *");
+            } else if (isNaN($("#height").val())) {
+                $("#height").addClass("is-invalid");
+                $("#height_err").html("Please provide valid height *");
             } else {
                 $("#height_err").html("");
                 $("#height").removeClass("is-invalid");
@@ -168,6 +240,9 @@
             if ($("#width").val() == 0) {
                 $("#width").addClass("is-invalid");
                 $("#width_err").html("Width cannot be 0 *");
+            } else if (isNaN($("#width").val())) {
+                $("#width").addClass("is-invalid");
+                $("#width_err").html("Please provide valid width *");
             } else {
                 $("#width_err").html("");
                 $("#width").removeClass("is-invalid");
@@ -178,6 +253,9 @@
             if ($("#length").val() == 0) {
                 $("#length").addClass("is-invalid");
                 $("#length_err").html("Length cannot be 0 *");
+            } else if (isNaN($("#length").val())) {
+                $("#length").addClass("is-invalid");
+                $("#length_err").html("Please provide valid height *");
             } else {
                 $("#length_err").html("");
                 $("#length").removeClass("is-invalid");
@@ -188,6 +266,9 @@
             if ($("#weight").val() == 0) {
                 $("#weight").addClass("is-invalid");
                 $("#weight_err").html("Weight cannot be 0 *");
+            } else if (isNaN($("#weight").val())) {
+                $("#weight").addClass("is-invalid");
+                $("#weight_err").html("Please provide valid weight *");
             } else {
                 $("#weight_err").html("");
                 $("#weight").removeClass("is-invalid");
@@ -198,6 +279,9 @@
             if ($("#size").val() == 0) {
                 $("#size").addClass("is-invalid");
                 $("#size_err").html("Size cannot be 0 *");
+            } else if (isNaN($("#size").val())) {
+                $("#size").addClass("is-invalid");
+                $("#size_err").html("Please provide valid size *");
             } else {
                 $("#size_err").html("");
                 $("#size").removeClass("is-invalid");
@@ -208,12 +292,32 @@
             if ($("#price").val() == 0) {
                 $("#price").addClass("is-invalid");
                 $("#price_err").html("Price cannot be 0 *");
+            } else if (isNaN($("#price").val())) {
+                $("#price").addClass("is-invalid");
+                $("#price_err").html("Please provide valid amount *");
             } else {
                 $("#price_err").html("");
                 $("#price").removeClass("is-invalid");
             }
         })
-
+        $("#sku").blur(function() {
+            if ($("#sku").val() == '') {
+                $("#sku").addClass("is-invalid");
+                $("#sku_err").html("Please provide valid sku *");
+            } else {
+                $("#sku_err").html("");
+                $("#sku").removeClass("is-invalid");
+            }
+        })
+        $("#name").blur(function() {
+            if ($("#name").val() == '') {
+                $("#name").addClass("is-invalid");
+                $("#name_err").html("Please provide valid name *");
+            } else {
+                $("#name_err").html("");
+                $("#name").removeClass("is-invalid");
+            }
+        })
         $("#height").keyup(function() {
             Height = $("#height").val();
             if (Height == "") {
@@ -300,47 +404,26 @@
         $("#sku").keyup(function() {
             sku = $("#sku").val();
             if (sku == "") {
+                $('#sku').addClass('is-invalid');
                 $("#sku_err").text("SKU is required");
             } else {
+                $("#sku").removeClass("is-invalid");
                 $("#sku_err").text("");
             }
         });
         $("#name").keyup(function() {
             name = $("#name").val();
             if (name == "") {
+                $('#name').addClass('is-invalid');
                 $("#name_err").text("Name is required");
             } else {
+                $("#name").removeClass("is-invalid");
                 $("#name_err").text("");
             }
         });
 
     });
 
-    const submitForm = () => {
-        var url = "<?php echo URLROOT; ?>/addproduct";
-        console.log(url);
-        data = {
-            sku: $("#sku").val(),
-            name: $("#name").val(),
-            price: $("#price").val(),
-            size: $("#size").val(),
-            type: $("#type").val(),
-            dvd: $("#dvd").val(),
-            height: $("#height").val(),
-            width: $("#width").val(),
-            length: $("#length").val(),
-            weight: $("#weight").val()
-        };
-        $.ajax({
-            type: "POST",
-            url: url,
-            data: data,
-            success: function(data) {
-                // alert(data);
-            }
-        });
-
-    }
     $(document).ready(function() {
         $('#productType').change(function() {
             var type = $(this).val();
@@ -348,14 +431,43 @@
                 $('.dvd-input').removeClass('d-none');
                 $('.furniture-input').addClass('d-none');
                 $('.book-input').addClass('d-none');
+                $("#length").removeClass("is-invalid");
+                $("#width").removeClass("is-invalid");
+                $("#height").removeClass("is-invalid");
+                $("#weight").removeClass("is-invalid");
+                if ($("#size").val() == "") {
+                    $("#size").addClass("is-invalid");
+                } else if (isNaN($("#size").val())) {
+                    $("#size").addClass("is-invalid");
+                }
             } else if (type == 3) {
+                $("#size").removeClass("is-invalid");
+                $("#weight").removeClass("is-invalid");
                 $('.furniture-input').removeClass('d-none');
                 $('.dvd-input').addClass('d-none');
                 $('.book-input').addClass('d-none');
+                if ($("#length").val() == "" || $("#width").val() == "" || $("#height").val() == "") {
+                    $("#length").addClass("is-invalid");
+                    $("#width").addClass("is-invalid");
+                    $("#height").addClass("is-invalid");
+                } else if (isNaN($("#length").val()) || isNaN($("#width").val()) || isNaN($("#height").val())) {
+                    $("#length").addClass("is-invalid");
+                    $("#width").addClass("is-invalid");
+                    $("#height").addClass("is-invalid");
+                }
             } else if (type == 2) {
+                $("#size").removeClass("is-invalid");
+                $("#length").removeClass("is-invalid");
+                $("#width").removeClass("is-invalid");
+                $("#height").removeClass("is-invalid");
                 $('.book-input').removeClass('d-none');
                 $('.dvd-input').addClass('d-none');
                 $('.furniture-input').addClass('d-none');
+                if ($("#weight").val() == "") {
+                    $("#weight").addClass("is-invalid");
+                } else if (isNaN($("#weight").val())) {
+                    $("#weight").addClass("is-invalid");
+                }
             }
         });
     });
