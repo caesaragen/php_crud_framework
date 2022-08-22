c784efcfad9101b04f515e98b1c5fb3a370aaeb6
caesaragen <caesaragen@gmail.com>
2022-08-22 20:47:32 +0300

    add changes

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -9,7 +9,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/css/style.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <!-- <script src="../public/js/main.js"></script> -->
+    <script src="../public/js/main.js"></script>
     <title><?php echo SITENAME; ?></title>
 </head>
 
